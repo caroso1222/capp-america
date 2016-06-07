54a64081418df67993021955b3e9729b620bfe08
Carlos <ce.roso398@gmail.com>
2016-06-07 18:44:30 -0500

    Added constants

---
+++ public/js/admin.js
@@ -0,0 +1,18 @@
+
+var app = angular.module('app',['ui.router']);
+
+
+app.config(function($stateProvider, $urlRouterProvider){
+	$urlRouterProvider.otherwise("/messages");
+	$stateProvider
+	.state('messages',{
+		url:"/messages",
+		templateUrl:"/templates/admin/messages.html",
+		controller:'messagesCtrl'
+	})
+	.state('config',{
+		url:"/config",
+		templateUrl:"/templates/admin/config.html",
+		controller:'configCtrl'
+	});
+});
